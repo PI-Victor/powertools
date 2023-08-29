a671ad14b69664dcfbc0100d50f514ca1643c0d6
Victor Palade <victor@cloudflavor.io>
2023-08-29 14:38:34 +0200

    powerutil: Send suspend signal via dbus.
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

--- crates/powerutil/src/main.rs
+++ crates/powerutil/src/main.rs
@@ -1,36 +1,37 @@
-#[allow(unstable_features)]
+use dbus::blocking::{BlockingSender, Connection};
+use dbus::Message;
 use serde::{Deserialize, Serialize};
 use std::ffi::CString;
 use std::path::{Path, PathBuf};
-use std::process::Command;
 use structopt::StructOpt;
 use utmp_rs::{Utmp32Parser, UtmpEntry};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Config {
+struct AppConfig {
     idle_threshold: i64,
     disable: bool,
     utmp_path: String,
 }
 
 #[derive(StructOpt)]
-enum Cli {
+struct Cli {
     Check(Check),
     Set(Set),
+    Install(Install),
 }
 
 #[derive(StructOpt)]
 struct Check {
     #[structopt(short, long, parse(from_os_str))]
     config_file: Option<std::path::PathBuf>,
-    #[structopt(short, long)]
-    view: bool,
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug, Clone)]
 struct Set {
+    #[structopt(short, long)]
+    init: bool,
     #[structopt(short, long, parse(from_os_str))]
     config_file: Option<std::path::PathBuf>,
     #[structopt(short, long)]
@@ -40,6 +41,14 @@ struct Set {
     #[structopt(short, long, parse(from_os_str), default_value = "/var/run/utmp")]
     utmp_path: std::path::PathBuf,
 }
+#[derive(Debug, StructOpt, Clone)]
+enum Config {
+    Set(Set),
+    View(View),
+}
+
+#[derive(StructOpt, Debug, Clone)]
+struct Install {}
 
 fn main() -> Result<()> {
     let opts = Cli::from_args();
@@ -47,20 +56,13 @@ fn main() -> Result<()> {
     match opts {
         Cli::Check(opts) => check_idle(opts),
         Cli::Set(opts) => set_config(opts),
+        Cli::Config(opts) => install_systemd_service(opts),
     }
 }
 
 fn check_idle(opts: Check) -> Result<()> {
     let config = get_config(opts.config_file)?;
 
-    if opts.view {
-        println!("Current config: {:#?}", &config);
-    }
-
-    if config.disable {
-        println!("Powerutil disable from config, exiting...");
-        std::process::exit(0);
-    }
     let mut idle_times = Vec::new();
 
     for entry in Utmp32Parser::from_path(config.utmp_path)? {
@@ -94,7 +96,9 @@ fn check_idle(opts: Check) -> Result<()> {
     );
     if *smallest_idle >= config.idle_threshold {
         println!("User idle threshold reached: {smallest_idle}m, suspending...");
-        suspend_system();
+        if let Err(error) = suspend_system() {
+            println!("Failed to suspend system: {error}");
+        }
     }
     Ok(())
 }
@@ -121,6 +125,17 @@ fn set_config(opts: Set) -> Result<()> {
     Ok(())
 }
 
+fn install_systemd_service(opts: CliConfig) -> Result<()> {
+    let path = if let Some(path) = opts.config_file {
+        path
+    } else {
+        let home_dir = std::env::var("HOME").unwrap_or(String::from("."));
+        PathBuf::from(home_dir).join(".powerutil.yaml")
+    };
+
+    Ok(())
+}
+
 fn get_config(config_file: Option<impl AsRef<Path>>) -> Result<Config> {
     let path = if let Some(path) = config_file {
         path.as_ref().to_path_buf()
@@ -136,11 +151,23 @@ fn get_config(config_file: Option<impl AsRef<Path>>) -> Result<Config> {
     Ok(config)
 }
 
-fn suspend_system() {
-    Command::new("systemctl")
-        .arg("suspend")
-        .output()
-        .expect("Failed to execute command");
+fn suspend_system() -> Result<Connection> {
+    let c = Connection::new_system()?;
+    let m = Message::new_method_call(
+        "org.freedesktop.login1",
+        "/org/freedesktop/login1",
+        "org.freedesktop.login1.Manager",
+        "Suspend",
+    )
+    .map_err(|e| format!("failed to create dbus message: {e}"))?
+    .append1(true);
+    let duration = std::time::Duration::from_secs(2);
+
+    let _ = c
+        .send_with_reply_and_block(m, duration)
+        .map_err(|e| format!("failed to suspend: {e}"))?;
+
+    Ok(c)
 }
 
 fn get_idle_time(tty_path: PathBuf) -> Result<libc::time_t> {
